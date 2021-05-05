dd625bed81a169a02e2c6b67034b8e4ade19e327
Jacky Lu <jackylu0937648512@gmail.com>
2021-05-05 23:13:09 +0800

    calc

--- read.py
+++ read.py
@@ -4,9 +4,12 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 1000 == 0:  # % = 餘數 , 1002 % 1000 = 2  1006 % 1000 = 6
 			print(len(data))
-print(len(data))
-print(data[0])
-print('----------------')
-print(data[1])
+print('總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度', sum_len/len(data))
